14cf8c1dd372c8b4f3a6839378518798e8908e32
nelfen <nelfen.dev@gmail.com>
2021-11-12 23:59:13 +0900

    21.11.12

--- FirstJava/src/Chapter02/DoubleEx2.java
+++ FirstJava/src/Chapter02/DoubleEx2.java
@@ -2,7 +2,7 @@ package Chapter02;
 
 public class DoubleEx2 {
 	public static void main(String[] args) {
-		double value = (1.0e20 + 5.0) - 1.0e20; //연산을 하면 5.0이 나와야 한다.
+		double value = (1.0e20 + 5.0) - 1.0e20; //원래 연산을 하면 5.0이 나와야 한다.
 		
 		System.out.println(value);
 	}

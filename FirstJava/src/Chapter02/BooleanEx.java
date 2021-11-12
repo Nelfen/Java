10d36f0496bf7abc286713015d3fb55abe41ab39
nelfen <nelfen.dev@gmail.com>
2021-11-12 23:19:30 +0900

    21.11.12 - BooleanEx

---
+++ FirstJava/src/Chapter02/BooleanEx.java
@@ -0,0 +1,8 @@
+package Chapter02;
+
+public class BooleanEx {
+	public static void main(String[] args) {
+		boolean isMarried = true; //boolean은 true 혹은 false만 반환합니다
+		System.out.println(isMarried);
+	}
+}

3cdf3280af23f3a423cad0e2edeac3be3468e41c
nelfen <nelfen.dev@gmail.com>
2021-11-12 23:34:59 +0900

    21.11.12 - DoubleEx1

---
+++ FirstJava/src/Chapter02/DoubleEx1.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class DoubleEx1 {
+
+	public static void main(String[] args) {
+		double dNum = 3.14123456789012345;
+		float fNum = 3.14123456789012345f;
+		
+		System.out.println(dNum);
+		System.out.println(fNum);
+	}
+
+}

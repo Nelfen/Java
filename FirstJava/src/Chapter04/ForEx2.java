42d7a9d0d6ca11935d942c3e891ce93a224aa1c8
nelfen <nelfen.dev@gmail.com>
2021-11-17 23:52:32 +0900

    21.11.17

---
+++ FirstJava/src/Chapter04/ForEx2.java
@@ -0,0 +1,12 @@
+package Chapter04;
+
+public class ForEx2 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("출력중..." + i);
+		}
+//		System.out.println(i);
+	}
+
+}

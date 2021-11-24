d91081e2d0196eaecfc5c3e4e00a9295e99c86cf
nelfen <nelfen.dev@gmail.com>
2021-11-24 23:42:16 +0900

    21.11.24

---
+++ FirstJava/src/Chapter05/Student2.java
@@ -0,0 +1,31 @@
+package Chapter05;
+
+public class Student2 {
+	public String studentName;
+	public int grade;
+	public int money;
+	
+	public Student2(String studentName, int money) {
+		this.studentName = studentName;
+		this.money = money;
+	}
+	
+	public void takeBus(Bus bus) {
+		bus.take(1000);
+		this.money -= 1000;
+	}
+	
+	public void takeSubway(Subway subway) {
+		subway.take(1500);
+		this.money -= 1500;
+	}
+	
+	public void takeTaxi(Taxi taxi) {
+		taxi.take(10000);
+		this.money -= 10000;
+	}
+	
+	public void showInfo() {
+		System.out.println(studentName + "님이 지불한 금액은 " + money + "입니다.");
+	}
+}

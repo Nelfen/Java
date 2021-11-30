35041aee27ca5008d9320929e35ba42164318da6
nelfen <nelfen.dev@gmail.com>
2021-11-30 23:26:53 +0900

    믹서기 예제

---
+++ FirstJava/src/Chapter08/Meat.java
@@ -0,0 +1,7 @@
+package Chapter08;
+
+public class Meat {
+	void changeJuice() {
+		System.out.println("다진고기가 되었습니다.");
+	}
+}
---
+++ FirstJava/src/Chapter08/Melon.java
@@ -0,0 +1,8 @@
+package Chapter08;
+
+public class Melon extends Fruit {
+	@Override
+	void changeJuice() {
+		System.out.println("멜론 주스를 만들었습니다.");
+	}
+}

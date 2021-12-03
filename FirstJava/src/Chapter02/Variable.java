54a95e1e763484c5fe64f5e9b84d523b5af2cfba
nelfen <nelfen.dev@gmail.com>
2021-12-04 00:48:19 +0900

    정수형 변수 사용법

--- FirstJava/src/Chapter02/Variable.java
+++ FirstJava/src/Chapter02/Variable.java
@@ -3,10 +3,10 @@ package Chapter02;
 public class Variable {
 
 	public static void main(String[] args) {
-		int level = 10; //level 변수를 선언하고 정수 10을 저장하였습니다.
+		int level = 10;
 		int hp;
-		System.out.println("현재 레벨은 얼마인가요?"); //문자열은 ""로 감싸고 화면에 그대로 출력됩니다.
-		System.out.println(level); //변수명을 입력하면 변수에 저장된 값이 출력됩니다.
+		System.out.println("현재 레벨은 얼마인가요?");
+		System.out.println(level);
 		System.out.println("현재 체력 게이지는 얼마인가요?");
 		hp = 500;
 		System.out.println(hp);

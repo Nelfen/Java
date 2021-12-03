f3082a6f556cea2a83700869aa0c6fddf9757d4c
nelfen <nelfen.dev@gmail.com>
2021-12-04 01:21:09 +0900

    반복문

--- FirstJava/src/Chapter04/ContinueEx.java
+++ FirstJava/src/Chapter04/ContinueEx.java
@@ -6,7 +6,7 @@ public class ContinueEx {
 		for(int i=0;i<5;i++) {
 			System.out.println("Hello!" + i);
 			if(i==1) {
-				continue; //이후의 문장을 수행하지 않고 증감식으로 넘어감. 현재의 반복을 중단하고 다음 반복 시작.
+				continue; //이후의 문장을 수행하지 않고 증감식으로 넘어감. 현재의 반복 중단후 다음 반복 시작.
 			}
 			System.out.println("출력..");
 		}

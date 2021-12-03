2f9dee13d72b6964ce3c8299fe9df67ef5481edf
nelfen <nelfen.dev@gmail.com>
2021-12-04 00:26:35 +0900

    불린 예제

--- FirstJava/src/Chapter02/BooleanEx.java
+++ FirstJava/src/Chapter02/BooleanEx.java
@@ -2,7 +2,7 @@ package Chapter02;
 
 public class BooleanEx {
 	public static void main(String[] args) {
-		boolean isMarried = true; //boolean은 true 또는 false만 반환합니다
+		boolean isMarried = true; //boolean은 true 또는 false만 반환합니다.
 		System.out.println(isMarried);
 	}
 }

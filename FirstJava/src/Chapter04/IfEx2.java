81533f285a9e1dd31b9957fd74911d1a166b708d
nelfen <nelfen.dev@gmail.com>
2021-12-06 22:59:09 +0900

    Scanner, if/switch-case Practice

--- FirstJava/src/Chapter04/IfEx2.java
+++ FirstJava/src/Chapter04/IfEx2.java
@@ -8,7 +8,7 @@ public class IfEx2 {
 		if (score >= 90) {
 			System.out.println("점수가 90보다 크거나 같습니다.");
 			System.out.println("등급은 A입니다.");
-		} else if  (score >= 80) {	//else 구문은 if문에서 false일 경우 실행되는 영역
+		} else if  (score >= 80) {	//else 구문은 if문에서 false일때 실행되는 영역
 			System.out.println("점수가 80보다 크거나 같습니다");
 			System.out.println("등급은 B입니다.");
 		} else if  (score >= 70) {

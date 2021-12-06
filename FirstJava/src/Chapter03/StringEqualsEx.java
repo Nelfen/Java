3c96958577634f3f76bb5971866fbac40809e4e6
nelfen <nelfen.dev@gmail.com>
2021-12-06 22:53:27 +0900

    비교 연산자, 문자열 비교(스택 영역/힙 영역), 복합 대입 연산자

--- FirstJava/src/Chapter03/StringEqualsEx.java
+++ FirstJava/src/Chapter03/StringEqualsEx.java
@@ -3,9 +3,9 @@ package Chapter03;
 public class StringEqualsEx {
 
 	public static void main(String[] args) {
-		String str1 = "실버";
-		String str2 = "실버";
-		String str3 = new String("실버");
+		String str1 = "자바";
+		String str2 = "자바";
+		String str3 = new String("자바");
 		
 		System.out.println(str1 == str2);
 		System.out.println(str2 == str3);

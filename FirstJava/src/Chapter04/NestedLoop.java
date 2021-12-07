d27641d7a14b3bb839f243b74de593c35f663a1c
nelfen <nelfen.dev@gmail.com>
2021-12-07 23:48:03 +0900

    반복문

--- FirstJava/src/Chapter04/NestedLoop.java
+++ FirstJava/src/Chapter04/NestedLoop.java
@@ -3,8 +3,6 @@ package Chapter04;
 public class NestedLoop {
 
 	public static void main(String[] args) {
-//		int dan;
-//		int times;
 		for(int i = 2; i <= 9; i++) {
 			for(int j = 1; j <= 9; j++) {
 				System.out.println(i + "X" + j + "=" + i * j);

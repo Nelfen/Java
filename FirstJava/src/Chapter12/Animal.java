b700c854f829a1b0d12e8a7394ab070c7acee02c
nelfen <nelfen.dev@gmail.com>
2021-12-18 23:53:49 +0900

    추상 메서드

---
+++ FirstJava/src/Chapter12/Animal.java
@@ -0,0 +1,10 @@
+package Chapter12;
+
+public abstract class Animal {
+	public String kind;
+	
+	public void breathe() {
+		System.out.println("¼ûÀ» ½±´Ï´Ù.");
+	}
+	public abstract void sound();
+}

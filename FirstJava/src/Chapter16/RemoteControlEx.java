5f0c30f89359b568d5a6944e93eba3c5deee991d
nelfen <nelfen.dev@gmail.com>
2021-12-31 23:08:28 +0900

    디폴트 메소드 사용(재정의), 정적 메소드 사용

---
+++ FirstJava/src/Chapter16/RemoteControlEx.java
@@ -0,0 +1,21 @@
+package Chapter16;
+
+public class RemoteControlEx {
+
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.setMute(true);
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.setMute(false);
+		rc.turnOff();
+		
+		RemoteControl.changeBattery();
+	}
+
+}

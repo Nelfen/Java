e2a7c508725905b2187e05f9c42dadbcceb91b58
nelfen <nelfen.dev@gmail.com>
2022-02-18 21:14:20 +0900

    Stack Example

---
+++ FirstJava/src/Chapter19/StackExample.java
@@ -0,0 +1,19 @@
+package Chapter19;
+
+import java.util.Stack;
+
+public class StackExample {
+	public static void main(String[] args) {
+		Stack<Coin> coinBox = new Stack<Coin>();
+		
+		coinBox.push(new Coin(100));
+		coinBox.push(new Coin(50));
+		coinBox.push(new Coin(500));
+		coinBox.push(new Coin(10));
+		
+		while(!coinBox.isEmpty()) {
+			Coin coin = coinBox.pop();
+			System.out.println("²¨³»¿Â µ¿Àü : " + coin.getValue() + "¿ø");
+		}
+	}
+}

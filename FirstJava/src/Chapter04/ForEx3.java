90a9034e984c0d606ee484c5f18b46dd403b5085
nelfen <nelfen.dev@gmail.com>
2022-02-24 23:01:20 +0900

    반복문

--- FirstJava/src/Chapter04/ForEx3.java
+++ FirstJava/src/Chapter04/ForEx3.java
@@ -4,8 +4,9 @@ public class ForEx3 {
 
 	public static void main(String[] args) {
 		int i;
+		int j;
 		for(i=0;i<3;i++) {
-			for(i=0;i<5;i++) {
+			for(j=0;j<5;j++) {
 				System.out.print("*");
 			}
 			System.out.println();

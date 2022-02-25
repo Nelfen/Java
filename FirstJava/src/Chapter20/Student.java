37d050aa76623c9841a488030a26ff2241e710e6
nelfen <nelfen.dev@gmail.com>
2022-02-25 23:50:43 +0900

    함수적 인터페이스 function

---
+++ FirstJava/src/Chapter20/Student.java
@@ -0,0 +1,17 @@
+package Chapter20;
+
+public class Student {
+	private String name;
+	private int englishScore;
+	private int mathScore;
+	
+	public Student(String name, int englisScore, int mathScore) {
+		this.name = name;
+		this.englishScore = englisScore;
+		this.mathScore = mathScore;
+	}
+	
+	public String getName() { return name; }
+	public int getEnglishScore() { return englishScore; }
+	public int getMathScore() { return mathScore; }
+}

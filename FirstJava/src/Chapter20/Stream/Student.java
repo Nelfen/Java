6863211e79eacf568ba219ea1593eeb1ce821bba
nelfen <nelfen.dev@gmail.com>
2022-03-24 23:01:29 +0900

    스트림

---
+++ FirstJava/src/Chapter20/Stream/Student.java
@@ -0,0 +1,29 @@
+package Chapter20.Stream;
+
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	public Student (String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}	
+	
+	@Override
+	public String toString() {
+		return name + "-" + score;
+	}
+	
+	@Override
+	public int compareTo(Student o) {
+		return Integer.compare(score,  o.score);
+	}
+}

71f0080cce6a2d6889b4c0f14a3482e64ef31dd8
nelfen <nelfen.dev@gmail.com>
2022-04-11 23:09:39 +0900

    try with resources

---
+++ FirstJava/src/Chapter21/AutoCloseObj.java
@@ -0,0 +1,10 @@
+package Chapter21;
+
+public class AutoCloseObj implements AutoCloseable {
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("¸®¼Ò½º°¡ close() µÇ¾ú½À´Ï´Ù.");
+	}
+	
+}

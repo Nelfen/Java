825fa300064119c0d6284130591dfe1d5669421b
nelfen <nelfen.dev@gmail.com>
2022-04-13 22:30:22 +0900

    try with resources

---
+++ FirstJava/src/Chapter21/AutoCloseTest.java
@@ -0,0 +1,17 @@
+package Chapter21;
+
+import java.util.Scanner;
+
+public class AutoCloseTest {
+	public static void main(String[] args) {
+		try(
+				AutoCloseObj obj = new AutoCloseObj();
+				Scanner sc = new Scanner(System.in);
+				){
+			int[] arr = new int[3];
+			arr[3] = 100;
+		} catch(Exception e) {
+			System.out.println("¿¹¿Ü ºÎºÐÀÔ´Ï´Ù.");
+		}
+	}
+}
